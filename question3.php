745a5283490977516ffbb122375a1fca3eb598e7
Malikakhon Kurmaeva U1910116 <m.kurmaeva@student.inha.uz>
2022-02-25 21:49:20 +0500

    question3

---
+++ question3.php
@@ -0,0 +1,12 @@
+<?php
+$student["Color"] = "red"; 
+$student["Name"] = "Maftuna"; 
+$student["Age"] = 21; 
+$student["ID"] = "U555777";
+
+echo "Characteristics for student are:\n"; 
+echo "Color:" . $student["Color"], "\n"; 
+echo "Name:" . $student["Name"], "\n"; 
+echo "Age:" . $student["Age"], "\n"; 
+echo "ID:" . $student["ID"], "\n"; 
+?>
